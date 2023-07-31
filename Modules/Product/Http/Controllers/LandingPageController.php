1762a8f36507a32bcd195d47d5ffef2066a72d2c
tpid-hendri <hendri@technopartner.co.id>
2023-07-31 09:20:39 +0700

    updateThird

--- Modules/Product/Http/Controllers/LandingPageController.php
+++ Modules/Product/Http/Controllers/LandingPageController.php
@@ -21,15 +21,18 @@ class LandingPageController extends Controller
     public function list(Request $request):JsonResponse
     {
         $post = $request->json()->all();
-        $category = $post['category'];
+        $category = $post['product_category_id'];
         $sortBy = $post['order_by'];
         $products = Product::with(['global_price', 'product_category'])
         ->when($category, function ($query) use ($category) {
-            return $query->where('product_category_id', $category);
+            if($category != 'all'){
+                return $query->where('product_category_id', $category);
+            }
         })
         ->when($sortBy, function ($query, $sortBy) {
             return $query->orderBy('product_name', $sortBy);
         })
+        ->firstOrFail()
         ->paginate(10);
         return $this->ok('success', $products);
     }
@@ -38,18 +41,8 @@ class LandingPageController extends Controller
     {
         $post = $request->json()->all();
         $id = $post['id'];
-        if(empty($id)){
-            return $this->error('ID Not found');
-        }
-        $products = Product::with(['global_price', 'product_category'])
-        ->when($id, function ($query) use ($id) {
-            return $query->where('id', $id);
-        })
-        ->get();
-        if(empty($products)){
-            return $this->error('Product Not Found');
-        }
-        return $this->ok('success', $products);
+        $products = Product::with(['global_price', 'product_category'])->where('id', $id)->firstOrFail();
+        return $this->ok("success", $products);
     }
 }
 
