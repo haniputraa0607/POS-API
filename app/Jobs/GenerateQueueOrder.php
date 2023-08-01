da7078170a3f99658df406d34e235fda54491ee8
tpid-hanif <tpid-hanif@technopartner.co.id>
2023-08-01 08:50:40 +0700

    update clean

--- app/Jobs/GenerateQueueOrder.php
+++ app/Jobs/GenerateQueueOrder.php
@@ -14,7 +14,11 @@ use Modules\Order\Entities\OrderProduct;
 
 class GenerateQueueOrder implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
     protected $data;
 
     /**
@@ -35,60 +39,56 @@ class GenerateQueueOrder implements ShouldQueue
         $outlet_id = $this->data['outlet_id'];
         $type = $this->data['type'];
 
-        if($type == 'product'){
-            $queue = OrderProduct::whereHas('order', function($order) use($order_id, $outlet_id){
+        if ($type == 'product') {
+            $queue = OrderProduct::whereHas('order', function ($order) use ($order_id, $outlet_id) {
                 $order->where('id', '<>', $order_id);
                 $order->whereDate('order_date', date('Y-m-d'));
                 $order->where('outlet_id', $outlet_id);
             })->where('type', 'Product')->max('queue') + 1;
 
-            if($queue<10){
-                $queue_code = 'P00'.$queue;
-            }elseif($queue<100){
-                $queue_code = 'P0'.$queue;
-            }else{
-                $queue_code = 'P'.$queue;
+            if ($queue < 10) {
+                $queue_code = 'P00' . $queue;
+            } elseif ($queue < 100) {
+                $queue_code = 'P0' . $queue;
+            } else {
+                $queue_code = 'P' . $queue;
             }
 
-            $update = OrderProduct::where('id', $order_product_id)->update(['queue'=>$queue,'queue_code'=>$queue_code]);
-
-        }elseif($type == 'treatment'){
-            $queue = OrderProduct::whereHas('order', function($order) use($order_id, $outlet_id){
+            $update = OrderProduct::where('id', $order_product_id)->update(['queue' => $queue,'queue_code' => $queue_code]);
+        } elseif ($type == 'treatment') {
+            $queue = OrderProduct::whereHas('order', function ($order) use ($order_id, $outlet_id) {
                 $order->where('id', '<>', $order_id);
                 $order->where('outlet_id', $outlet_id);
             })->whereDate('schedule_date', date('Y-m-d', strtotime($this->data['schedule_date'])))
             ->where('type', 'Treatment')
             ->max('queue') + 1;
 
-            if($queue<10){
-                $queue_code = 'T00'.$queue;
-            }elseif($queue<100){
-                $queue_code = 'T0'.$queue;
-            }else{
-                $queue_code = 'T'.$queue;
+            if ($queue < 10) {
+                $queue_code = 'T00' . $queue;
+            } elseif ($queue < 100) {
+                $queue_code = 'T0' . $queue;
+            } else {
+                $queue_code = 'T' . $queue;
             }
 
-            $update = OrderProduct::where('id', $order_product_id)->update(['queue'=>$queue,'queue_code'=>$queue_code]);
-
-        }elseif($type == 'consultation'){
-            $queue = OrderConsultation::whereHas('order', function($order) use($order_id, $outlet_id){
+            $update = OrderProduct::where('id', $order_product_id)->update(['queue' => $queue,'queue_code' => $queue_code]);
+        } elseif ($type == 'consultation') {
+            $queue = OrderConsultation::whereHas('order', function ($order) use ($order_id, $outlet_id) {
                 $order->where('id', '<>', $order_id);
                 $order->where('outlet_id', $outlet_id);
             })->whereDate('schedule_date', date('Y-m-d', strtotime($this->data['schedule_date'])))
             ->where('doctor_id', $this->data['doctor_id'])
             ->max('queue') + 1;
 
-            if($queue<10){
-                $queue_code = 'C00'.$queue;
-            }elseif($queue<100){
-                $queue_code = 'C0'.$queue;
-            }else{
-                $queue_code = 'C'.$queue;
+            if ($queue < 10) {
+                $queue_code = 'C00' . $queue;
+            } elseif ($queue < 100) {
+                $queue_code = 'C0' . $queue;
+            } else {
+                $queue_code = 'C' . $queue;
             }
 
-            $update = OrderConsultation::where('id', $order_product_id)->update(['queue'=>$queue,'queue_code'=>$queue_code]);
-
+            $update = OrderConsultation::where('id', $order_product_id)->update(['queue' => $queue,'queue_code' => $queue_code]);
         }
-
     }
 }
