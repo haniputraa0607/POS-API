5ab717e55d473dd1a54d0f2e55732455bc2981a3
tpid-danang <134244296+tpid-danang@users.noreply.github.com>
2023-08-14 16:45:47 +0700

    feat: - menambahakan migrasi 'daviena_logs' untuk logging database - setup 'db_log' channel untuk logging database

---
+++ database/migrations/2023_08_14_084346_create_daviena_logs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::connection(env('SECOND_DB_DRIVER'))->dropIfExists('daviena_logs');
+        Schema::connection(env('SECOND_DB_DRIVER'))->create('daviena_logs', function (Blueprint $table) {
+            $table->id();
+            $table->longText('message');
+            $table->longText('context');
+            $table->string('level')->index();
+            $table->string('level_name');
+            $table->string('channel')->index();
+            $table->string('record_datetime');
+            $table->longText('extra');
+            $table->longText('formatted');
+            $table->string('remote_addr')->nullable();
+            $table->string('user_agent')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('daviena_logs');
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -12,15 +12,21 @@
 */
 
 use App\Http\Controllers\AccessTokenController;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 // header('Access-Control-Allow-Origin:  *');
 header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
-Route::controller( AccessTokenController::class)->prefix('/login')->group(function(){
+Route::controller(AccessTokenController::class)->prefix('/login')->group(function () {
     Route::post('cms', 'loginCMS')->name('login.cms');
     Route::post('doctor', 'loginDoctor')->name('login.doctor');
     Route::post('cashier', 'loginCashier')->name('login.cashier');
 });
 
 Route::get('/logout', [AccessTokenController::class, 'logout'])->name('logout')->middleware('auth:api');;
+
+Route::get('test-log', function () {
+    Log::channel('db_log')->info("test log debug test", ['message' => 'test logging from url', "run"]);
+    return ["result" => true];
+});
