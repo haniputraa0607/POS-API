883e314df72eb87da98bc0942f4ae96d6537a2e8
tpid-hanif <tpid-hanif@technopartner.co.id>
2023-08-28 16:53:23 +0700

    update api doctor

--- database/seeders/SettingTableSeeder.php
+++ database/seeders/SettingTableSeeder.php
@@ -33,6 +33,15 @@ class SettingTableSeeder extends Seeder
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),
             ),
+            2 =>
+            array(
+                'id' => 3,
+                'key' => 'active_payment_methods',
+                'value' => NULL,
+                'value_text' => NULL,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ),
         );
 
         foreach ($rows as $row) {
