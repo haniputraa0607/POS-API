104a3046605d89f4527db41c97000aa3db6514a8
tpid-hendri <hendri@technopartner.co.id>
2023-10-05 16:23:42 +0700

    updateFirstBranch

--- app/Lib/MyHelper.php
+++ app/Lib/MyHelper.php
@@ -199,4 +199,19 @@ class MyHelper
 
         return $log;
     }
+
+    function haversineDistance($lat1, $lon1, $lat2, $lon2)
+    {
+        $lat1 = deg2rad($lat1);
+        $lon1 = deg2rad($lon1);
+        $lat2 = deg2rad($lat2);
+        $lon2 = deg2rad($lon2);
+        $dlat = $lat2 - $lat1;
+        $dlon = $lon2 - $lon1;
+        $a = sin($dlat / 2) ** 2 + cos($lat1) * cos($lat2) * sin($dlon / 2) ** 2;
+        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+        $radius = 6371;
+        $distance = $radius * $c;
+        return $distance;
+    }
 }
