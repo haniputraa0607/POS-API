7ee7abfc67b2f48fb93338c3fbcec73ba87152af
danang prasetyo <danang@technopartner.co.id>
2023-10-13 14:00:36 +0700

    fix: - update migration employee_schedule_dates table

--- Modules/Cashier/Database/Migrations/2023_10_12_100343_change_column_employee_schedule_dates.php
+++ Modules/Cashier/Database/Migrations/2023_10_12_100343_change_column_employee_schedule_dates.php
@@ -13,7 +13,23 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('outlet_schedule_shifts', function (Blueprint $table) {
+            $table->after('shift_time_end', function (Blueprint $table) {
+                $table->time('start_break')->nullable();
+                $table->time('end_break')->nullable();
+            });
+        });
+
+        Schema::table('employee_schedule_dates', function (Blueprint $table) {
+            $table->dropColumn('shift');
+            $table->dropColumn('time_start');
+            $table->dropColumn('time_end');
+
+            $table->after('date', function (Blueprint $table) {
+                $table->foreignId('outlet_schedule_shift_id')->constrained('outlet_schedule_shifts');
+            });
+
+        });
     }
 
     /**
@@ -23,6 +39,23 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('outlet_schedule_shifts', function (Blueprint $table) {
+            $table->dropColumn('start_break');
+            $table->dropColumn('end_break');
+        });
+
+        Schema::table('employee_schedule_dates', function (Blueprint $table) {
+            $table->after('date', function (Blueprint $table) {
+                $table->enum('shift',config('outlet_shift'))->default('Morning');
+                $table->time('time_start')->nullable();
+                $table->time('time_end')->nullable();
+            });
+        });
+
+        Schema::table('employee_schedule_dates', function (Blueprint $table) {
+            $table->dropForeign('employee_schedule_dates_outlet_schedule_shift_id_foreign');
+            $table->dropColumn('outlet_schedule_shift_id');
+        });
+        
     }
 };
