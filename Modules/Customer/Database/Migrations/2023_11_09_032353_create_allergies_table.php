f35684c7c69ad7e7d9ff0bd1414bc10a746a3b44
tpid-hanif <tpid-hanif@technopartner.co.id>
2023-11-09 11:06:08 +0700

    update api allergy

---
+++ Modules/Customer/Database/Migrations/2023_11_09_032353_create_allergies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('allergies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained('category_allergies');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('allergies');
+    }
+};
---
+++ Modules/Customer/Entities/CustomerAllergy.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modules\Customer\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Modules\Customer\Entities\Allergy;
+use Modules\Customer\Entities\Customer;
+
+class CustomerAllergy extends Model
+{
+    use HasFactory;
+
+    protected $table = 'allergies';
+    protected $fillable = [
+        'allergy_id',
+        'customer_id',
+        'notes'
+    ];
+
+    public function allergy(): BelongsTo
+    {
+        return $this->belongsTo(Allergy::class);
+    }
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+}
